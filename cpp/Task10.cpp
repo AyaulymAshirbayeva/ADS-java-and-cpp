8a00713824ac8f7b5bb6f1b7b64e05ced45b7685
ayaulym <251174@astanait.edu.kz>
2026-03-24 21:19:57 +0500

    git add

---
+++ cpp/Task10.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+bool palindrome(string s, int l, int r) {
+    if (l >= r) return true;
+    if (s[l] != s[r]) return false;
+    return palindrome(s, l + 1, r - 1);}
+int main() {
+    string s;
+    cin >> s;
+    if (palindrome(s, 0, s.size() - 1))
+        cout << "Palindrome";
+    else
+        cout << "Not palindrome";}
